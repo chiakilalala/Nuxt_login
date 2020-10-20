5faa6ebdece15ee027a1becf65d90a4cb5e7514d
yilin <chiaki205102@gmail.com>
2020-10-20 13:09:23 +0800

    config.js upload

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,31 +1,29 @@
 module.exports = {
     apps: [{
-
-        script: './node_modules/nuxt-start/bin/nuxt-start.js',
-        instances: 'max', //負載平衡模式下的 cpu 數量
-        exec_mode: "cluster", //cpu 負載平衡模式
-        max_memory_restart: '1G', //緩存了多少記憶體重新整理
-
-        env_prod: {
-            "name": 'chiaki_nuxt_prod',
-            "PORT": 3001, //指定伺服器上的 port
-            "NODE_ENV": "prod"
+            name: 'chiakilalala_prod',
+            script: './node_modules/nuxt-start/bin/nuxt-start.js',
+            instances: 'max', //負載平衡模式下的 cpu 數量
+            exec_mode: "cluster", //cpu 負載平衡模式
+            max_memory_restart: '1G', //緩存了多少記憶體重新整理
+            port: 3001
         },
-        env_sit: {
-            "name": 'chiaki_nuxt_sit',
-            "PORT": 3002, //指定伺服器上的 port
-            "NODE_ENV": "sit"
+        {
+            name: 'chiakilalala_sit',
+            script: './node_modules/nuxt-start/bin/nuxt-start.js',
+            instances: 'max', //負載平衡模式下的 cpu 數量
+            exec_mode: "cluster", //cpu 負載平衡模式
+            max_memory_restart: '1G', //緩存了多少記憶體重新整理
+            port: 3002
         }
-
-    }],
+    ],
     deploy: {
         "prod": {
             "user": "chiakilalala", //linux 登入帳號 帳號@ip
             "host": ["35.201.148.166"], //你的伺服器 ip
-            "ref": "origin/master", //分支
+            "ref": "origin/main", //分支
             "repo": "git@gitlab.com:chiaki205102/nuxt_demo.git", //ssh 的 git
             "path": "/home/chiakilalala/nuxt_demo", //伺服器上的路徑
-            "post-deploy": "npm install && npm run build && pm2 reload ecosystem.config.js --env prod", //佈署指令
+            "post-deploy": "npm install && npm run build && pm2 reload ecosystem.config.js --env prod --only chiakilalala_prod", //佈署指令
             "env": {
                 "NODE_ENV": "prod"
             }
